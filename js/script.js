a7c8c75bd460ca0b994ed78ff9f650d2431f7187
Daniel McDonagh <dmcdonagh@glenman.ie>
2025-03-03 16:29:44 +0000

    new flipping albums

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-
 let currentAudio = null;
+
 function flipAlbum(element) {
     element.classList.toggle('flipped');
 }
@@ -7,16 +7,16 @@ function flipAlbum(element) {
 function toggleAudio(event, songSrc) {
     event.stopPropagation(); // Prevent album flip when clicking the song
     let audio = document.getElementById("audioPlayer");
+
     if (currentAudio === songSrc) {
         if (!audio.paused) {
             audio.pause();
             audio.currentTime = 0;
             currentAudio = null;
-         }
+        }
     } else {
         audio.src = songSrc;
         audio.play();
         currentAudio = songSrc;
     }
-}    
-
+}
